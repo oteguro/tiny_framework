ece1ec097d1625046c78ca6236371df3683614d4
Tai Yamaguchi <oteguro@hotmail.com>
2017-09-12 02:04:43 +0900

    add various files.

---
+++ include/tiny_base.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#if defined(_DEBUG)
+#define TF_DEBUG                (1)
+#endif
+
+#define TF_PLATFORM_WINDOWS     (1)
+#define TF_PLATFORM_MSVC        (1)
+
+
+namespace tf
+{
+
+
+
+} // namespace tf 
+
+
+
+
